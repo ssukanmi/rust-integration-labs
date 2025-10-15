1370b6ea3ab29a17fd467f2f9875013b76d45623
Olayinka Olasunkanmi <sj.olayinka@gmail.com>
2025-10-15 09:31:32 -0400

    Add colored terminal output to stats display

--- pipeviewer/src/stats.rs
+++ pipeviewer/src/stats.rs
@@ -1,5 +1,13 @@
 use crossbeam::channel::Receiver;
-use std::{io::Result, time::{Duration, Instant}};
+use crossterm::{
+    cursor, execute,
+    style::{self, Color, PrintStyledContent, Stylize},
+    terminal::{Clear, ClearType},
+};
+use std::{
+    io::{self, Result, Stderr, Write},
+    time::{Duration, Instant},
+};
 
 trait TimeOutput {
     fn as_time(&self) -> String;
@@ -7,8 +15,8 @@ trait TimeOutput {
 
 impl TimeOutput for u64 {
     fn as_time(&self) -> String {
-        let (hours, left) = (*self/3600, *self%3600);
-        let (mins, secs) = (left/60, left%60);
+        let (hours, left) = (*self / 3600, *self % 3600);
+        let (mins, secs) = (left / 60, left % 60);
         format!("{}:{:02}:{:02}", hours, mins, secs)
     }
 }
@@ -48,6 +56,7 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
     let mut timer = Timer::new();
+    let mut stderr = io::stderr();
 
     while let Ok(num_bytes) = stats_rx.recv() {
         timer.update();
@@ -55,11 +64,11 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
         total_bytes += num_bytes;
         if !silent && timer.ready {
             timer.ready = false;
-            eprint!(
-                "\r{} {} [{:.0}b/s]",
+            output_progress(
+                &mut stderr,
                 total_bytes,
                 start.elapsed().as_secs().as_time(),
-                rate_per_second
+                rate_per_second,
             );
         }
 
@@ -73,6 +82,21 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     }
 
     Ok(())
+}
+
+fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64) {
+    let bytes = style::style(format!("{} ", bytes)).with(Color::Red);
+    let elapsed = style::style(elapsed).with(Color::Green);
+    let rate = style::style(format!(" [{:.0}b/s]", rate)).with(Color::Blue);
+    let _ = execute!(
+        stderr,
+        cursor::MoveToColumn(0),
+        Clear(ClearType::CurrentLine),
+        PrintStyledContent(bytes),
+        PrintStyledContent(elapsed),
+        PrintStyledContent(rate)
+    );
+    let _ = stderr.flush();
 }
 
 pub fn stats(silent: bool, num_read: usize, total_bytes: &mut usize, last: bool) {
